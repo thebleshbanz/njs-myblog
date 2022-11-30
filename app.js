cdd07ab7b998514c2f9f1ba011dcbae72e9cfc3b
thebleshbanz <bleshbanz@gmail.com>
2022-11-30 10:26:04 +0530

    myblog : server & seeds

--- app.js
+++ app.js
@@ -1,32 +1,93 @@
-var createError = require('http-errors');
+/**
+ * Express is a node js web application framework that provides broad features for building web and mobile applications.
+ */
 var express = require('express');
+
+
+/**
+ * Get a PATH containing locally installed module executables.
+ */
 var path = require('path');
+
+/**
+ * cookie-parser is a middleware which parses cookies attached to the client request object.
+ */
 var cookieParser = require('cookie-parser');
+
+/**
+ * HTTP request logger middleware for node.js.
+ */
 var logger = require('morgan');
-const mongoose = require('mongoose')
+
+/**
+ * decreases the downloadable amount of data that's served to users.
+ * improvement in the performance of the application.
+ * reduce the payload size dramatically above 70%.
+ */
+const compression = require('compression')
+
+/**
+ * helmet - helps you secure your Express apps by setting various HTTP headers.
+ */
+const helmet = require('helmet')
+
+/**
+ * Cross-Origin Resource Sharing
+ * It is a mechanism to allow or restrict requested resources on a web server depend on where the HTTP request was initiated.
+ */
+const cors = require('cors')
+
+/**
+ * passport - Express-compatible authentication middleware
+ * Main purpose is to authenticate requests
+ */
+const passport = require('passport')
+
+/**
+ * Lightweight simple translation module with dynamic JSON storage
+ */
+const i18n = require('i18n')
+
+const initMongo = require('./config/mongo')
+
+/**
+ * Initialize App
+ */
+var app = express();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+// i18n
+i18n.configure({
+  locales: ['en', 'es'],
+  directory: `${__dirname}/locales`,
+  defaultLocale: 'en',
+  objectNotation: true
+})
+app.use(i18n.init)
 
+// Enable only in development HTTP request logger middleware
+app.use(logger('dev'));
+app.use(cors());
+app.use(passport.initialize());
+/**
+ * express.json - used to parse the incoming requests with JSON payloads and is based upon the bodyparser */
+app.use(express.json());
+app.use(express.static('public'))
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
+app.use(compression())
+app.use(helmet())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -39,8 +100,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-/* mongoose.connect('mongodb+srv://blog_demo_nodejs:uHZTpzyCKlirboce@blog-demo-nodejs.b9pgir8.mongodb.net/?retryWrites=true&w=majority')
-     .then(() => console.log('MongoDB Database Connected'))
-     .catch(err => console.log(err)) */
+
+// Init MongoDB
+initMongo()
 
 module.exports = app;
---
+++ config/mongo.js
@@ -0,0 +1,40 @@
+/**
+ * Mongoose is a Object Data Modeling (ODM) library for MongoDB distributed as an npm package
+ */
+const mongoose = require('mongoose')
+
+const DB_URL = process.env.MONGO_URI || "mongodb+srv://ngs-myblog:NuDrRHtK7quS3Cpn@myblog.tfrtibr.mongodb.net/?retryWrites=true&w=majority=test";
+
+// const loadModels = require('../app/models')
+
+/* mongoose.connect('mongodb+srv://blog_demo_nodejs:uHZTpzyCKlirboce@blog-demo-nodejs.b9pgir8.mongodb.net/?retryWrites=true&w=majority')
+     .then(() => console.log('MongoDB Database Connected'))
+     .catch(err => console.log(err)) */
+
+module.exports = () => {
+  const connect = () => {
+    mongoose.Promise = global.Promise
+
+    mongoose.connect(
+      DB_URL,
+      {
+        keepAlive: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      },
+      (err) => {
+        let dbStatus = ''
+        if (err) {
+          dbStatus = `*    Error connecting to DB: ${err}\n****************************\n`
+        }
+        dbStatus = `*    DB Connection: OK\n****************************\n`
+      }
+    )
+  }
+  connect()
+
+  mongoose.connection.on('error', console.log)
+  mongoose.connection.on('disconnected', connect)
+
+//   loadModels()
+}
